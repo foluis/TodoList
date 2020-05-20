d797f6bd44ca36224cfb9e2059e8958a667bc730
Luis Forero <foluis@hotmail.com>
2020-05-19 22:35:02 -0500

    Test Services with postman

--- Code/Na.TodoList/Na.TodoList.Database/Dbo/Script/Post-Deploy/InitialTasks.sql
+++ Code/Na.TodoList/Na.TodoList.Database/Dbo/Script/Post-Deploy/InitialTasks.sql
@@ -1,5 +1,5 @@
 ﻿IF((SELECT COUNT(1) FROM [dbo].[Todos]) = 0)
 BEGIN
-	INSERT INTO [dbo].[[Todos] 
+	INSERT INTO [dbo].[Todos] 
 	SELECT 'Implement Entity Framework',GETDATE(),DATEADD(DAY,3,GETDATE()),0
 END
